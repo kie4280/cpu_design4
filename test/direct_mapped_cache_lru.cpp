89019cafe13fa91210a5c8d57a79b9eda6ce6437
kie4280 <kie4280ann@gmail.com>
2020-06-08 22:33:51 +0800

    1. seems to be right 2. add advance_data excel

--- test/direct_mapped_cache_lru.cpp
+++ test/direct_mapped_cache_lru.cpp
@@ -24,53 +24,58 @@ void simulate(int cache_size, int block_size, int n_way) {
 
   int offset_bit = (int)log2(block_size);
   int set_bit = (int)log2(n_way);
-  int index_bit = (int)log2(cache_size / block_size) - set_bit;
+  int index_bit = (int)log2(cache_size / block_size);
 
-  int line = cache_size >> (offset_bit) >> set_bit; // rows
+  int line = (cache_size >> offset_bit); // rows
   int accesses = 0, miss = 0;
 
-  cache_content **cache = new cache_content*[line];
-  for (int a = 0; a < line; ++a) {
+  cache_content **cache = new cache_content*[line-set_bit];
+  for (int a = 0; a < line-set_bit; ++a) {
     cache[a] = new cache_content[n_way];
   }
 
   // cout << "cache line: " << line << endl;
 
-  for (int j = 0; j < line; j++) {
+  for (int j = 0; j < line-set_bit; j++) {
     for (int i = 0; i < n_way; ++i) {
       cache[j][i].v = false;
     }
   }
 
-  FILE *fp = fopen("test/LU.txt", "r");  // read file
+  FILE *fp = fopen("test/RADIX.txt", "r");  // read file
 
   while (fp != 0 && fscanf(fp, "%x", &x) != EOF) {
     // cout << hex << x << " ";
     index = (x >> offset_bit) & (line - 1);
     tag = x >> (index_bit + offset_bit);
+    unsigned int set_index = index >> set_bit;
     unsigned int least_used = 1000000;
     int least_index = 0;
     
     for (int b = 0; b <= n_way; ++b) {
-      if (b < n_way && cache[index][b].v && cache[index][b].tag == tag) {
-        cache[index][b].v = true;  // hit
-        cache[index][b].count++;
+      if (b < n_way && cache[set_index][b].v && cache[set_index][b].tag == tag) {
+        cache[set_index][b].v = true;  // hit
+        cache[set_index][b].count++;
+        break;
       }      
 
       else if(b == n_way){
-        cache[index][least_index].count = 1;
-        cache[index][least_index].v = true;
-        cache[index][least_index].tag = tag;
+        for(int j=0; j<n_way; ++j) {
+          if(least_used > cache[set_index][j].count) {
+            least_used = cache[set_index][j].count;
+            least_index = j;
+          }
+        }
+        cache[set_index][least_index].count = 1;
+        cache[set_index][least_index].v = true;
+        cache[set_index][least_index].tag = tag;
         miss++;
       }
 
-      if(b<n_way && least_used > cache[index][b].count) {
-        least_used = cache[index][b].count;
-        least_index = b;
-      }
+      
       
     }
-
+    // cout <<dec<< miss << " " << accesses << endl;
     accesses++;
   }
   if (fp != 0) {
@@ -79,7 +84,7 @@ void simulate(int cache_size, int block_size, int n_way) {
 
   // cout <<dec<< miss << " " << accesses << endl;
   cout << (double)miss / accesses << endl;
-  for (int a = 0; a < line; ++a) {
+  for (int a = 0; a < line-set_bit; ++a) {
     delete[] cache[a];
   }
   delete[] cache;
@@ -87,7 +92,9 @@ void simulate(int cache_size, int block_size, int n_way) {
 
 int main() {
   // Let us simulate 4KB cache with 16B blocks
-  
-    simulate(256 * K, 64, 2);
+    for(int a=0; a<=6; ++a) {
+      simulate(128 * K, 64, 1<<a);
+    }
+    
   
 }
