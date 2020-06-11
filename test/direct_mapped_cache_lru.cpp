30398934070206a064ea3ff83b8bdc76edce1102
kie4280 <kie4280ann@gmail.com>
2020-06-11 13:47:51 +0800

    1. change LRU to remove longest unused

--- test/direct_mapped_cache_lru.cpp
+++ test/direct_mapped_cache_lru.cpp
@@ -49,15 +49,17 @@ void simulate(int cache_size, int block_size, int n_way) {
     index = (x >> offset_bit) & (line - 1);
     tag = x >> (index_bit + offset_bit);
     unsigned int set_index = index & ((line>>set_bit)-1);
+    // unsigned int set_index = index >> set_bit;
     
-    unsigned int least_used = 10000000;
+    unsigned int least_used = 0;
     int least_index = 0;
 
     for (int b = 0; b <= n_way; ++b) {
       if (b < n_way && cache[set_index][b].v &&
           cache[set_index][b].tag == tag) {
         cache[set_index][b].v = true;  // hit
-        cache[set_index][b].count++;
+        cache[set_index][least_index].count = 0;
+        
         break;
       }
 
@@ -67,16 +69,22 @@ void simulate(int cache_size, int block_size, int n_way) {
             least_index = j;
             break;
           }
-          if (least_used >= cache[set_index][j].count) {
+          if (least_used < cache[set_index][j].count) {
             least_used = cache[set_index][j].count;
             least_index = j;
           }
         }
-        cache[set_index][least_index].count = 1;
+        cache[set_index][least_index].count = 0;
         cache[set_index][least_index].v = true;
         cache[set_index][least_index].tag = tag;
         miss++;
       }
+
+      
+    }
+
+    for(int b=0; b<n_way; ++b) {
+      cache[set_index][b].count++;
     }
     // cout <<dec<< miss << " " << accesses << endl;
     accesses++;
